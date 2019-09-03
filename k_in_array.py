d414caf442f33986360160488754c6769d02aa32
Shankar <52577232+AVShankar@users.noreply.github.com>
2019-09-03 20:59:38 +0530

    Update k_in_array.py

--- k_in_array.py
+++ k_in_array.py
@@ -1,8 +1,12 @@
 #Given 2 numbers N and K followed by n numbers check if K exists
 
+count=0
 N, K = map(int, input().split())
 n=list(map(int, input().split()[:N]))
 for i in range (0, N):
     if(n[i]==K):
         print("yes")
+        count+=1
         break
+if(count==0):
+        print("no")
