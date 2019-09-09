e762647636fb1866ff12089e723c0b7653d104db
Shankar <52577232+AVShankar@users.noreply.github.com>
2019-09-09 16:43:34 +0530

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,22 @@
+#Given 2 numbers N and K followed by n numbers print the number of repetitions of K
+
+N, M = map(int, input().split())
+result1 = []
+result2 = []
+for i in range(1, N + 1):
+       if N % i == 0:
+           result1.append (i)
+
+for i in range(1, M + 1):
+       if M % i == 0:
+           result2.append (i)
+
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+intercept = intersection(result1, result2)
+final = 1
+for i in intercept:
+    final = final * i
+
+print(final)
