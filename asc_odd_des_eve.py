c916fc914b7da41e55798c1f6579d41d45f8f628
Shankar <52577232+AVShankar@users.noreply.github.com>
2019-09-10 23:37:19 +0530

    Create asc_odd_des_eve.py

---
+++ asc_odd_des_eve.py
@@ -0,0 +1,21 @@
+#Sorting
+
+N = int(input())
+n = list(map(int, input().split()[:N]))
+for i in range (0, N):
+    if(i%2 == 0):
+        for j in range (2, N, 2):
+            if(n[j] < n[j-2]):
+                if(i != N-1):
+                    temp = n[j]
+                    n[j] = n[j-2]
+                    n[j-2] = temp
+    else: 
+        for j in range (3, N, 2):
+            if(n[j] > n[j-2]):
+                if(i != N-1):
+                    temp = n[j]
+                    n[j] = n[j-2]
+                    n[j-2] = temp
+for i in range (0, N):
+    print(n[i], end=" ")
